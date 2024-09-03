6fe2533dd205193e72c994c5c5509fc4d051688a
Smokvin <alex.smokvin@gmail.com>
2024-09-03 19:02:28 +0300

    Init commit

---
+++ src/test/java/com/example/vacationpaycalculation/VacationPayCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacationpaycalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPayCalculationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
